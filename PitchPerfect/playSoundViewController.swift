05571215b71ef73ded5cb186474e8d6caaf793a0
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-08-24 12:20:39 +0300

    second window full graphic

--- PitchPerfect/playSoundViewController.swift
+++ PitchPerfect/playSoundViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class playSoundViewController: UIViewController {
     var recordedAudioURL: URL!
-
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +32,16 @@ class playSoundViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+
+    
 
     /*
     // MARK: - Navigation
@@ -32,5 +52,4 @@ class playSoundViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
+   }
