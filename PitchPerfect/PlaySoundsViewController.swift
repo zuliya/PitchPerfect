4669ec8f522e1691122bbc8c6011dcf74ce281b9
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-09-14 11:58:45 +0100

    OMG IT all WORKS

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -30,15 +30,31 @@ class PlaySoundsViewController: UIViewController {
     
     
     enum ButtonType: Int {
-        case slow = 0, fast, chipmunk, vader, echo, reverb
+        case slow = 0, fast = 1, chipmunk = 2, vader = 3, echo = 4, reverb = 5
     }
+   
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play Sound Button Pressed")
+       switch(ButtonType(rawValue: sender.tag)!)
+       {
+       case .slow:
+        playSound(rate: 0.5)
+       case .fast:
+        playSound(rate: 1.5)
+       case .chipmunk:
+        playSound(pitch: 1000)
+       case .vader:
+        playSound(pitch: -1000)
+       case .echo:
+        playSound(echo: true)
+       case .reverb:
+        playSound(reverb: true)
+        }
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("Stop Audio Button Pressed")
+        stopAudio()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
